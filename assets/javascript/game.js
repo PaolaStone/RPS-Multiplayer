98d251099d3ab6e7f995a099df9283d2b6984ad6
Paola Stone <paola.stonemartinez@dell.com>
2019-08-12 23:17:48 -0400

    players status added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,15 +17,18 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-var player1Name = ""
-var selection1 = ""
-var wins1 = 0
-var losses1 = 0
+var player1Status = "notLogged";
+var player1Name = "";
+var selection1 = "";
+var wins1 = 0;
+var losses1 = 0;
 
-var player2Name = ""
-var selection2 = ""
-var wins2 = 0
-var losses2 = 0
+
+var player2Status = "notLogged";
+var player2Name = "";
+var selection2 = "";
+var wins2 = 0;
+var losses2 = 0;
 
 var playersRef = database.ref("/players");
 var connectionsRef = database.ref("/connections");
@@ -34,6 +37,7 @@ var chatRef = database.ref("/chat");
 // console.log("this is connected  "+ connectedRef)
 
 player1Object = {
+    player1Status : "",
     player1Name: "",
     player1Choice: "",
     wins1: 0,
@@ -41,27 +45,12 @@ player1Object = {
 }
 
 player2Object = {
+    player2Status : "",
     player2Name: "",
     player2Choice: "",
     wins2: 0,
     losses2: 0,
 }
-
-connectedRef.on("value", function(snap){
-    if (snap.val()){
-        var con = connectionsRef.push(true);
-        con.onDisconnect().remove();
-    }
-});
-
-connectionsRef.on("value", function(snap){
-    console.log("this is the number of connections  "+ snap.numChildren());
-    // if (((snap.numChildren()) === 2) && (player1Name !== "")&&(player2Name !=="")){
-    //     console.log("all if conditions are true")
-    // }else{
-    //     $("#result").append("Waiting on other player")
-    // }
-})
 
 
 
@@ -115,15 +104,36 @@ function chatShow() {
     x.style.display = "block";
 }
 
-function selectPlayerHide() {
-    var x = document.getElementById("selectPlayer");
+// function selectPlayerHide() {
+//     var x = document.getElementById("selectPlayer");
+//     x.style.display = "none";
+// }
+
+// function selectPlayerShow() {
+//     var x = document.getElementById("selectPlayer");
+//     x.style.display = "block";
+// }
+
+function selected1Hide() {
+    var x = document.getElementById("selected1");
     x.style.display = "none";
 }
 
-function selectPlayerShow() {
-    var x = document.getElementById("selectPlayer");
+function selected1Show() {
+    var x = document.getElementById("selected1");
     x.style.display = "block";
 }
+
+function selected2Hide() {
+    var x = document.getElementById("selected2");
+    x.style.display = "none";
+}
+
+function selected2Show() {
+    var x = document.getElementById("selected2");
+    x.style.display = "block";
+}
+
 
 function player1FormHide() {
     var x = document.getElementById("player1Form");
@@ -165,21 +175,31 @@ function shoot2Show() {
     x.style.display = "block";
 }
 
-gameAreatHide();
+// gameAreatHide();
 
-$("#selected1").on("click", function(){
-    event.preventDefault();
-    gameAreaShow();
-    selectPlayerHide();
-    resultHide();
+connectedRef.on("value", function(snap){
+    if (snap.val()){
+        var con = connectionsRef.push(true);
+        con.onDisconnect().remove();
+    }
+});
+
+function selected1 (){
+   // gameAreaShow();
+    selected2Hide();
+    // player2FormHide();
     container2Hide();
+    resultHide();
     chatHide();
+    selected1Show();
+    player1FormShow();
     shoot1Hide();
     
     $("#player1Name").on("click", function(){
         event.preventDefault();
         player1Name = $("#player1-input").val().trim();
         database.ref().push({
+            player1Status: "logged",
             player1Name:player1Name,
         });
         player1FormHide();
@@ -189,33 +209,106 @@ $("#selected1").on("click", function(){
         chatShow();
 
     });
-    
-});
+};
 
-$("#selected2").on("click", function(){
-    event.preventDefault();
+function selected2 (){
+    
     gameAreaShow();
-    selectPlayerHide();
+    selected1Hide();
+    player1FormHide();
     resultHide();
     container1Hide();
     chatHide();
     shoot2Hide();
+    selected2Show();
+    player2FormShow();
     
     $("#player2Name").on("click", function(){
         event.preventDefault();
         player2Name = $("#player2-input").val().trim();
         database.ref().push({
+            player2Status: "logged",
             player2Name:player2Name,
         });
         player2FormHide();
         $("#player2").append(player2Name + "  ,you are player 2");
-        shoot2Show();
+        shoot1Show();
         resultShow();
         chatShow();
 
     });
+};
+
+connectionsRef.on("value", function(snap){
+    console.log("this is the number of connections  "+ snap.numChildren());
+    console.log("p1 status before if  " + player1Status)
+    if (snap.numChildren() === 1 && player1Status === "notLogged"){
+        console.log("display player 1 picture and form");
+        selected1 ();
+        player1Status = "logged"
+        console.log("p1 status bafter if  " + player1Status)
+    }
+    console.log("p2 status before if  " + player2Status)
+    if (snap.numChildren() === 2 && player1Status === "notLogged") {
+        console.log("display player 2 picture and form");
+        selected2 ();
+        player2Status = "logged"
+        console.log("p2 status after if  " + player2Status)
+    }
     
-});
+})
+
+
+
+// $("#selected1").on("click", function(){
+//     event.preventDefault();
+//     gameAreaShow();
+//     selectPlayerHide();
+//     resultHide();
+//     container2Hide();
+//     chatHide();
+//     shoot1Hide();
+    
+//     $("#player1Name").on("click", function(){
+//         event.preventDefault();
+//         player1Name = $("#player1-input").val().trim();
+//         database.ref().push({
+//             player1Name:player1Name,
+//         });
+//         player1FormHide();
+//         $("#player1").append(player1Name + "  ,you are player 1");
+//         shoot1Show();
+//         resultShow();
+//         chatShow();
+
+//     });
+    
+// });
+
+// $("#selected2").on("click", function(){
+//     event.preventDefault();
+//     gameAreaShow();
+//     selectPlayerHide();
+//     resultHide();
+//     container1Hide();
+//     chatHide();
+//     shoot2Hide();
+    
+//     $("#player2Name").on("click", function(){
+//         event.preventDefault();
+//         player2Name = $("#player2-input").val().trim();
+//         database.ref().push({
+//             player2Name:player2Name,
+//         });
+//         player2FormHide();
+//         $("#player2").append(player2Name + "  ,you are player 2");
+//         shoot2Show();
+//         resultShow();
+//         chatShow();
+
+//     });
+    
+
 
 
 
